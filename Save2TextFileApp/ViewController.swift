00f35b8ee9377742d396b20ce66c4f72244827bc
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-15 14:15:08 -0500

    Initial Commit

---
+++ Save2TextFileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Save2TextFileApp
+//
+//  Created by student on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
